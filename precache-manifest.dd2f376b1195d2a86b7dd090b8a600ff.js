8294898f661170a57ad53d02153623938a9732f2
Adrienne Kerr <akerr@lmtc-adkerr.jomax.paholdings.com>
2019-12-22 20:30:39 -0700

    add build

---
+++ precache-manifest.dd2f376b1195d2a86b7dd090b8a600ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ca373b34996192397eb6238673d914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99436e8a999101e42e36",
+    "url": "/static/css/main.4fe36669.chunk.css"
+  },
+  {
+    "revision": "c247ec140266af012918",
+    "url": "/static/js/2.011fe35b.chunk.js"
+  },
+  {
+    "revision": "99436e8a999101e42e36",
+    "url": "/static/js/main.e4c2d85f.chunk.js"
+  },
+  {
+    "revision": "bcd26a3da2fcc0baf790",
+    "url": "/static/js/runtime-main.cd5b930e.js"
+  }
+]);
